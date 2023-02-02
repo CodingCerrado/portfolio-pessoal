b897249bb597ecefdcda9d005712545173883522
Jefferson Silva <111249250+Jeffersonlpu@users.noreply.github.com>
2023-02-02 12:32:58 -0300

    add-push-to-dataform

--- paginas/contact/contact.js
+++ paginas/contact/contact.js
@@ -8,24 +8,23 @@ const assunto = document.querySelector(".ct-input-subject");
 const mensagem = document.querySelector(".ct-input-message");
 const button = document.querySelector(".ct-submit-button");
 
-form.addEventListener("submit", (e) => {
-  alert("funcionou!");
-  e.preventDefault();
-  const dataForm = new FormData(form);
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  for (const item of form) {
+//   const dataForm = new FormData(form);
+
+//   for (const item of form) {
+//     console.log(item.value);
+//   }
+// });
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const dataForm = [];
+
+  for (let item of form) {
+    dataForm.push(item.value)
     console.log(item.value);
   }
 });
-
-function checkInputs() {
-  const nameValue = nome.value;
-  const surnameValue = sobrenome.value;
-  const emailValue = email.value;
-  const subjectValue = assunto.value;
-  const messageValue = mensagem.value;
-
-  // if (emailValue === "") {
-  //   console.log("Preencha os dados!");
-  // }
-}
